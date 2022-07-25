ccfaca00e3696725f9c6ad2e0cd6c8729963f81e
kyle-olivier20 <98287478+kyle-olivier20@users.noreply.github.com>
2022-07-25 11:18:51 -0600

    Update login_check.py

--- login_check.py
+++ login_check.py
@@ -9,7 +9,6 @@ class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(),
-                                                                    EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password', validators=[
+        DataRequired(), EqualTo('password')])
     submit = SubmitField('Log In')
-    
