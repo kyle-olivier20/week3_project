baaaa500f83c3bf541e6cc6f8caa0bd201245c24
kyle-olivier20 <98287478+kyle-olivier20@users.noreply.github.com>
2022-07-25 11:22:54 -0600

    Update sp2.py

--- sp2.py
+++ sp2.py
@@ -4,13 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('home.html', subtitle='Home Page', 
+    return render_template('home.html', subtitle='Home Page',
                            text='This is the home page')
 
 
 @app.route("/second_page")
 def second_page():
-    return render_template('second_page.html', subtitle='Second Page', text='This is the second page')
+    return render_template('second_page.html', subtitle='Second Page',
+                           text='This is the second page')
 
 
 if __name__ == '__main__':
